265a0109826d11b31d44f8e3ca2fe47e32e83968
Simone Giertz <simone@giertz.com>
2014-01-22 11:28:52 +0100

    fixed incorrect query install + new fantasy_images with proper proportions + arrows for navigation

--- js/main.js
+++ js/main.js
@@ -1,16 +1,27 @@
 var fantasyImagesArray=["thinking_about_sm.svg","thinking_about_dressing_up.svg","thinking_about_group_sex.svg","thinking_about_masturbating.svg","thinking_about_roleplay.svg","thinking_about_watching.svg","thinking_about_nothing.svg"]
-var counter=1;
-
+var counter=0;
+console.log("testing")
 $(document).ready(function(){
-$( "#fantasy_examples" ).click(function() {
-if(counter<fantasyImagesArray.length){
-	$( "#fantasy_examples" ).html( "<img src='fantasy_images/"+fantasyImagesArray[counter]+"' width='200px'>" );
+$( "#right_arrow").click(function() {
   counter+=1;
+if(counter<fantasyImagesArray.length){
+	$( "#fantasy_examples" ).html( "<img src='fantasy_images/"+fantasyImagesArray[counter]+"' width='300px'>" );
   }
   else{
-  	console.log("else")
- 	$( "#fantasy_examples" ).html( "<img src='fantasy_images/"+fantasyImagesArray[0]+"' width='200px'>" );
+ 	$( "#fantasy_examples" ).html( "<img src='fantasy_images/"+fantasyImagesArray[0]+"' height='300px'>" );
   	counter=1;
   }
 });
+$( "#left_arrow").click(function() {
+	counter-=1
+if(counter!=-1){
+	$( "#fantasy_examples" ).html( "<img src='fantasy_images/"+fantasyImagesArray[counter]+"' width='300px'>" );
+  }
+  else{
+  	counter=fantasyImagesArray.length-1
+ 	$( "#fantasy_examples" ).html( "<img src='fantasy_images/"+fantasyImagesArray[counter]+"' height='300px'>" );
+  }
+
+});
+
 });

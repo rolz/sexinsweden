c051894a61fc2ab90a56281cd9ff31b9fcbf2d31
Jonathan <jonathan.lonnqvist@gmail.com>
2014-01-22 19:56:31 +0100

    Cleaned up the functions

--- js/components/screw.js
+++ js/components/screw.js
@@ -9,50 +9,36 @@ var ctx = c.getContext("2d");    // The height of the canvas
 
 $('#screw').click(canvasApp);
 
-  function canvasApp() {
-    window.setInterval(drawScreen, 1);
+function canvasApp() {
+  window.setInterval(drawScreen, 1);
+}
+    
+function drawScreen() {
+  drawImageRot(scrImg,0,0,canvasHeight,canvasWidth,degrees);
+}
+
+function drawImageRot(img,x,y,width,height,deg){
+  if (degrees == 361) {
+      degrees = 0
+      // return;
+  } else {
+      degrees += 1
+      ctx.clearRect(0,0, 120, 120)
   }
     
-  function drawScreen() {
+  //Convert degrees to radian 
+  var rad = deg * Math.PI / 180;
 
-    
-    // console.log(ctx);
-    
-      drawImageRot(scrImg,0,0,canvasHeight,canvasWidth,degrees);
-    }
+  //Set the origin to the center of the image
+  ctx.translate(x + width / 2, y + height / 2);
 
-    function drawImageRot(img,x,y,width,height,deg){
-      if (degrees == 361) {
-          return;
-          degrees = 0
-        } else {
-          degrees += 1
-          ctx.clearRect(0,0, 120, 120)
-        }
-        
-        //Convert degrees to radian 
-        var rad = deg * Math.PI / 180;
+  //Rotate the canvas around the origin
+  ctx.rotate(rad);
 
-        //Set the origin to the center of the image
-        ctx.translate(x + width / 2, y + height / 2);
+  //draw the image    
+  ctx.drawImage(img,width / 2 * (-1),height / 2 * (-1),width,height);
 
-        //Rotate the canvas around the origin
-        ctx.rotate(rad);
-
-        //draw the image    
-        ctx.drawImage(img,width / 2 * (-1),height / 2 * (-1),width,height);
-
-        
-
-        //reset the canvas  
-        ctx.rotate(rad * ( -1 ) );
-        ctx.translate((x + width / 2) * (-1), (y + height / 2) * (-1));
-        
-        
-        console.log(degrees);
-
-    }
-
-
-  
-// });
+  //reset the canvas  
+  ctx.rotate(rad * ( -1 ) );
+  ctx.translate((x + width / 2) * (-1), (y + height / 2) * (-1));
+}

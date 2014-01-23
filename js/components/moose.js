1df88681d5546527a23a3127dc767c2b91f5d7bc
Simone Giertz <simone@giertz.com>
2014-01-23 13:03:35 +0100

    more effective way of drawing antlers + cleaned up code. still a bit buggy and a callback needs to be added

--- js/components/moose.js
+++ js/components/moose.js
@@ -74,7 +74,7 @@ window.onload = function(){
 			ctx.moveTo(sp.x, H-sp.y);
 			ctx.lineTo(ep2.x, H-ep2.y);
 			
-			drawnLines.push({start_X1:sp.x, start_Y1:H-sp.y, end_X1:ep1.x, end_Y1:H-ep1.y, start_X2:sp.x, start_Y2:H-sp.y, end_X2:ep2.x, end_Y2:H-ep2.y, draw_width:line_width+2})
+			drawnLines.push({start_X1:sp.x, start_Y1:H-sp.y, end_X1:ep1.x, end_Y1:H-ep1.y, start_X2:sp.x, start_Y2:H-sp.y, end_X2:ep2.x, end_Y2:H-ep2.y, draw_width:line_width})
 			//Time to make this function recursive to draw more branches
 			ep1.angle = sp.angle+divergence;
 			ep2.angle = sp.angle-divergence;
@@ -82,10 +82,12 @@ window.onload = function(){
 			new_start_points.push(ep1);
 			new_start_points.push(ep2);
 		}
+		if(length > 50) ctx.strokeStyle = "black";
+		else ctx.strokeStyle = "white";
 		ctx.stroke();
 		start_points = new_start_points;
 
-		if(length > age*10) branches ();		
+		if(length > 2) branches ();		
 	}
 	
 	function get_endpoint(x, y, a, length)
@@ -95,50 +97,71 @@ window.onload = function(){
 		return {x: epx, y: epy};
 	}
 
-function remove_lines(number){
-var barrier=drawnLines.length-1
-var barrier2=drawnLines.length/2-1
+function ageLines(number, color){
+	number=number/10
+	console.log(number)
 
-for (var n = barrier; n >=barrier2 ; n--) {
-	ctx.beginPath();
-	ctx.lineWidth=drawnLines[n].draw_width;
-    ctx.moveTo(drawnLines[n].end_X2, drawnLines[n].end_Y2);
-	ctx.lineTo(drawnLines[n].start_X2, drawnLines[n].start_Y2);
-	ctx.moveTo(drawnLines[n].end_X1, drawnLines[n].end_Y1);
-	ctx.lineTo(drawnLines[n].start_X1, drawnLines[n].start_Y1);
-	ctx.strokeStyle="white";
-	ctx.stroke();
-	drawnLines.pop()
-};
+	if(color=="black"){
+	var barrier=Math.pow(2,number)
+	var barrier2=Math.pow(2,number+1)
+
+	for (var n = barrier-1; n <barrier2-1 ; n++) {
+		ctx.beginPath();
+		ctx.lineWidth=drawnLines[n].draw_width;
+	    ctx.moveTo(drawnLines[n].end_X2, drawnLines[n].end_Y2);
+		ctx.lineTo(drawnLines[n].start_X2, drawnLines[n].start_Y2);
+		ctx.moveTo(drawnLines[n].end_X1, drawnLines[n].end_Y1);
+		ctx.lineTo(drawnLines[n].start_X1, drawnLines[n].start_Y1);
+		ctx.strokeStyle=color;
+		ctx.stroke();
+	};
+	}
+	else{
+		var barrier=Math.pow(2,number)
+		var barrier2=Math.pow(2,number+1)
+		for (var n = barrier2-2; n >barrier-2 ; n--) {
+			ctx.beginPath();
+			ctx.lineWidth=drawnLines[n].draw_width*5;
+		    ctx.moveTo(drawnLines[n].end_X2, drawnLines[n].end_Y2);
+			ctx.lineTo(drawnLines[n].start_X2, drawnLines[n].start_Y2);
+			ctx.moveTo(drawnLines[n].end_X1, drawnLines[n].end_Y1);
+			ctx.lineTo(drawnLines[n].start_X1, drawnLines[n].start_Y1);
+			ctx.strokeStyle=color;
+			ctx.stroke();
+	};
+
+}
 //number = parseInt(number, 10)
 //exponent = number/10;
 //
 //var startPointsRemoved= Math.pow(2, exponent)
 //console.log("startpointsremoved: "+startPointsRemoved)
 
-var barrier=start_points.length/2+1
-
-for (var n = 0; n <barrier-1; n++) {
-	start_points.pop()
-};
+//var barrier=start_points.length/2+1
+//
+//for (var n = 0; n <barrier-1; n++) {
+//	start_points.pop()
+//};
 
 
 }
 $(document).ready(function() {
   $("[name=range]").on("change", function() {
     $("[for=range]").val(this.value);
-  age=this.value
+    previousAge=age
+  	age=this.value
+  	console.log("previousAge: "+previousAge+" age: "+age)
   if(age%10==0&&previousAge<age){
-   	previousAge=age
   	console.log("add lines")
-	branches();
+	ageLines(age,"black");
+
   }
-  else if(age!=100 && age%10==0 && previousAge>age){
-   	previousAge=age
+  else if(age%10==0 && previousAge>age){
   	console.log("remove lines")
-  	remove_lines(age);
+  	ageLines(age,"white");
+
   }
-  
+
   }).trigger("change");
 
   

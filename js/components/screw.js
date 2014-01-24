f18bb8eb9026ae55de39cf64c8300a7af1de8ca9
Jonathan <jonathan.lonnqvist@gmail.com>
2014-01-24 12:44:05 +0100

    Less repeated instructions

--- js/components/screw.js
+++ js/components/screw.js
@@ -1,74 +1,39 @@
+//Elements
+var c = document.getElementById("screw");
+var d = document.getElementById("screwtext");
+var more = document.getElementById("more");
+var less = document.getElementById("less");
+
 //Declaring variables
 var canvasWidth = 300;        // The width of the canvas
 var canvasHeight = canvasWidth;       // The height of the canvas
 var radian = Math.PI/180;
-var speed = 1;
+var speed = 10;
 var center = canvasWidth/2;
 var lW = canvasWidth/50;
 var lineEnd = canvasWidth/4
 var radCirc = center - lW;
-var c = document.getElementById("screw");
-var d = document.getElementById("screwtext");
 var ctx = c.getContext("2d");    
 var degrees = 0;
 window.onload = canvasApp();
 c.onclick = drawRevolution;
+// less.onclick = unscrew;
 var times = 0;
-d.innerHTML = "<h1>"+times+"</h1>";
+var status = 1;
+var angle = 0;
 
 function canvasApp() {
-  drawScreen();
+  resetCanvas();
+  drawScrew();
 }
-    
-function drawScreen() {
-  // reset canvas
-  ctx.setTransform(1,0,0,1,0,0);
-  ctx.clearRect(0,0, 300, 300);
+
+function drawScrew() {
+  
   // draw circle
   ctx.beginPath();
   ctx.strokeStyle = "black";
   ctx.lineWidth = lW;
   ctx.arc(center, center, radCirc, radian*0, radian*360, false)
-  
-  ctx.stroke();
-  ctx.closePath();
-
-  // set center
-  ctx.translate(center, center);
-
-  // draw cross
-  ctx.lineWidth = Math.floor(lW*3)
-  ctx.lineCap = "round";
-  drawLine(0,0,lineEnd,0);
-  drawLine(0,0,0,lineEnd);
-  drawLine(lineEnd*-1,0,0,0);
-  drawLine(0,lineEnd*-1,0,0);
-
-};
-
-function drawRevolution() {
-  if (degrees == 360) {
-      degrees = 0
-      times += 1
-      d.innerHTML = "<h1>"+times+"</h1>";
-      clearInterval(timer);
-      return null;
-  } else if (degrees == 0) {
-      timer= setInterval(function(){drawRevolution()},speed);
-      degrees += 1
-      var angle = degrees * radian;
-  } else {
-    degrees += 1
-    var angle = degrees * radian;
-  }
-  
-  ctx.setTransform(1,0,0,1,0,0);
-  ctx.clearRect(0,0, canvasWidth, canvasHeight);
-  ctx.beginPath();
-  ctx.strokeStyle = "black";
-  ctx.lineWidth = lW;
-    ctx.arc(center, center, radCirc, radian*0, radian*360, false)
-  //full circle
   ctx.stroke();
   ctx.closePath();
 
@@ -83,6 +48,29 @@ function drawRevolution() {
   drawLine(0,0,0,lineEnd);
   drawLine(lineEnd*-1,0,0,0);
   drawLine(0,lineEnd*-1,0,0);
+}
+
+function drawRevolution() {
+
+  if (degrees == 360 || degrees == -360) {
+    degrees = 0;
+    clearInterval(timer);
+    // timer= setInterval(function(){drawRevolution()},speed);
+    // return null;
+
+  } else if (degrees == 0) {
+    times += 1;
+    d.innerHTML = "<h1>"+times+"</h1>";
+    timer= setInterval(function(){drawRevolution()},speed);
+    degrees += 2
+    angle = degrees * radian;
+  
+  } else {
+    degrees += 2
+    angle = degrees * radian;
+  }
+  resetCanvas();
+  drawScrew();
 };
 
 function drawLine(startX, startY, endX, endY) {
@@ -91,4 +79,12 @@ function drawLine(startX, startY, endX, endY) {
   ctx.lineTo(endX, endY);
   ctx.stroke();
   ctx.closePath();
+}
+
+function resetCanvas() {
+  var h=c.height;
+  var w=c.width;
+  c.width=w;
+  c.height=h;
+  ctx.setTransform(1,0,0,1,0,0);
 }

3c7699992b7ce6d12c1e38418ed7ec208948f2a5
Simone Giertz <simone@giertz.com>
2014-01-26 16:35:52 +0100

    shit

--- js/components/positions.js
+++ js/components/positions.js
@@ -7,6 +7,13 @@ $(document).ready(function ($) {
     itemSelector: '.item',
     isFitWidth: true
   });
+
+  var $container = $('.enough-sex-grid').masonry({
+    columnWidth: 420,
+    itemSelector: '.enough-sex-item',
+    isFitWidth: true
+  });
+
       
   $('.item').click(function () { 
     $(this).toggleClass('expand');

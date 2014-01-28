e6f3d81f93f7c8558c6edc8b3b57b491cfd35549
Simone Giertz <simone@giertz.com>
2014-01-28 17:02:37 +0100

    started adding help dude

---
+++ js/components/help.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function ($) {
+
+  $('.help').click(function () {
+    var thisId="#"+$(this).attr('id')+"-help"
+console.log(thisId)
+    $(thisId).toggleClass('help-visible')
+      //console.log(thisClass)
+      //$( thisClass ).html( '<img src="assets/img/help/'+thisId+'.svg" style="z-index:1000; position:absolute" height="200px">' );
+
+
+  });
+
+
+
+});

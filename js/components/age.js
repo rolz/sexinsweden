48c3e20c2b300afb1478a62e1c365282e93519ea
Simone Giertz <simone@giertz.com>
2014-01-29 21:51:48 +0100

    started on drop down

--- js/components/age.js
+++ js/components/age.js
@@ -1,4 +1,5 @@
 var ageArray = ["Stop screwing around, you're too young", 5.6, 5.28, 5.55, 4.13, 3.84, 4.08, 2.65, 1.91]
+var ageArrayValue;
 
 // Single and lack a stable relationship	1.49
 // Student	5.05
@@ -12,33 +13,55 @@ function getAge() {
     speed1 = 0.4;
     if (age < 17) {
     	speed1 = 0; 
-    	f.innerHTML = ageArray[0];
+        ageArrayValue=0;
     } else if (age >=18 && age <=20) {
     	speed1 = speed1 + 0.6;
-    	f.innerHTML = ageArray[1];
+        ageArrayValue=1;
     } else if (age >= 21 && age <=25) {
     	speed1 = speed1 + 0.5;
-    	f.innerHTML = ageArray[2];
+        ageArrayValue=2;
     } else if (age >= 26 && age <=30) {
     	speed1 = speed1 + 0.6;
-    	f.innerHTML = ageArray[3];
+        ageArrayValue=3;
     } else if (age >= 31 && age <=40) {
     	speed1 = speed1 + 0.4;
-    	f.innerHTML = ageArray[4];
+        ageArrayValue=4;
     } else if (age >= 41 && age <=50) {
     	speed1 = speed1 + 0.4;
-    	f.innerHTML = ageArray[5];
+        ageArrayValue=5;
     } else if (age >= 51 && age <=60) {
     	speed1 = speed1 + 0.4;
-    	f.innerHTML = ageArray[6];
+        ageArrayValue=6;
     } else if (age >= 61 && age <=69) {
     	speed1 = speed1 + 0.3;
-    	f.innerHTML = ageArray[7];
+        ageArrayValue=7;
     } else if (age >= 70 && age <=100) {
     	speed1 = speed1 + 0.2;
-    	f.innerHTML = ageArray[8];
+        ageArrayValue=8;
     }
+    f.innerHTML = ageArray[ageArrayValue];
     rotSpeed2 = setInterval(function(){drawSweden()},1);
 	rotSpeed2;
 };
 
+//drop down menu change
+$(document).ready(function() {
+    $('select#group-choice').change(function() {
+
+    console.log("change")
+    var value= $(this).context.value
+    if(value=="Students"){
+       f.innerHTML="5.05";
+    }
+    else if(value=="Singles"){
+        f.innerHTML="1.49";
+    }
+    else if(value=="Swedes who are 70+"){
+        f.innerHTML="1.91";
+    }
+    else if(value=="Swedes your age"){
+        f.innerHTML=ageArray[ageArrayValue];
+    }
+
+})
+});

5ab68ffcbe9a4303479c5ebebad22e81f645fc5a
rolz <zrolland@gmail.com>
2014-02-22 14:30:17 +0100

    tracking up to question 3

--- js/analytics/tracking.js
+++ js/analytics/tracking.js
@@ -1,9 +1,55 @@
+//////////////////
 //QUIZ
 
-//Question 1 - Gender
+//Question 0 - Gender
 $('.q0').children('img')
     .each(function(){
         $(this).on('click', function() {
-          ga('send', 'event', 'Quiz', 'Gender', $(this).attr('id'));
+          ga('send', 'event', 'Quiz', 'Q0 - Gender', $(this).attr('id'));
     });
 });
+
+//Question 1 - Prudeness
+$('.q1').find('button')
+    .each(function(){
+        $(this).on('click', function() {
+          ga('send', 'event', 'Quiz', 'Q1 - Prudeness', $(this).attr('id'));
+    });
+});
+
+//Question 2 - Virginity
+$('.q2').find('button')
+    .each(function(){
+        $(this).on('click', function() {
+          ga('send', 'event', 'Quiz', 'Q2 - Virginity', $(this).attr('id'));
+    });
+});
+
+//Question 3 - Sex per month
+// ***get value out
+$('.q3').find('button')
+    .each(function(){
+        $(this).on('click', function() {
+          ga('send', 'event', 'Quiz', 'Q2 - Virginity', $(this).attr('id'));
+    });
+});
+
+
+
+
+
+//////////////////
+
+//AGE
+
+//Moose
+
+
+////////////////
+// Time spent on sections
+
+
+///////////////
+//Explorations clicks
+
+//Funnels
